cb5c30b5b16bfe6ae69683050f386ea569f8b989
Rathod125 <tusharrathod5801@gmail.com>
2023-10-13 01:11:53 -0400

    Initial Commit

---
+++ Lab5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab5
+//
+//  Created by user234887 on 10/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
